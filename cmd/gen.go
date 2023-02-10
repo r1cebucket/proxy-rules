e963f98535a892151900ad331d2b4ce1730b8a1f
Ricebucket <88389934+r1cebucket@users.noreply.github.com>
2023-02-10 20:42:05 +0000

    update

---
+++ cmd/gen.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var MODES []string
+	var domainProxy []string
+	var domainDirect []string
+
+	MODES = []string{
+		"clash",
+		"quan x",
+		"matsuri",
+	}
+
+	// PROXY
+	PROXY := "amp-api-edge.apps.apple.com push.apple.com inappcheck.itunes.apple.com app-measurement.com nexoncdn.co.kr nexon.com nexon.io "
+	// Crusaders Quest
+	PROXY += "nhn.com adam.gslb.toastoven.net "
+	domainProxy = strings.Split(PROXY, " ")
+
+	// DIRECT
+	DIRECT := ""
+	// Microsoft
+	DIRECT += "microsoft.com microsoftonline.com sharepoint.com office.com office.net "
+	// Crusaders Quest
+	DIRECT += "cq.hangame.com cq-pvp.hangame.com cq-cha.hangame.com gslb-gamebase.nhncloudservice.com " // ios
+	DIRECT += "nhnst.com "
+	domainDirect = strings.Split(DIRECT, " ")
+
+	for _, MODE := range MODES {
+		SaveConfig(domainProxy, domainDirect, MODE)
+	}
+}
+
+func SaveConfig(domainProxy, domainDirect []string, MODE string) {
+	var rule string
+	switch MODE {
+	case "quan x":
+		conf, err := os.Create("./rules/quanx.conf")
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		defer conf.Close()
+
+		for _, domain := range domainProxy[:len(domainProxy)-1] {
+			rule = fmt.Sprintf("HOST-SUFFIX,%s,PROXY\n", domain)
+			conf.Write([]byte(rule))
+		}
+		for _, domain := range domainDirect[:len(domainDirect)-1] {
+			rule = fmt.Sprintf("HOST-SUFFIX,%s,DIRECT\n", domain)
+			conf.Write([]byte(rule))
+		}
+	case "matsuri":
+		confProxy, err := os.Create("./rules/matsuri_proxy.conf")
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		confDirect, err := os.Create("./rules/matsuri_direct.conf")
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		defer confProxy.Close()
+		defer confDirect.Close()
+
+		for _, domain := range domainProxy[:len(domainProxy)-1] {
+			rule = fmt.Sprintf("domain:%s\n", domain)
+			confProxy.Write([]byte(rule))
+		}
+		for _, domain := range domainDirect[:len(domainDirect)-1] {
+			rule = fmt.Sprintf("domain:%s\n", domain)
+			confDirect.Write([]byte(rule))
+		}
+	default:
+		fmt.Println("no such mode:", MODE)
+	}
+}
--- cmd/gen.py
+++ cmd/gen.py
@@ -1,26 +1,42 @@
-# -*- coding: utf-8 -*-
-import requests
-
-mode  = 'clash'
-mode = 'quan x'
+MODE  = 'clash'
+MODE = 'quan x'
 urls_proxy = []
 urls_direct = []
 
+DIRECT = ""
 # DIRECT
-s = '163.com bilibili.com bilivideo.cn bilivideo.com biliapi.net baidu.com 189.cn aliyundrive.com zhihu.com amap.com huya.com jd.com taobao.com miobt.com'
-urls_direct.extend(s.split(' '))
+## Microsoft
+DIRECT += "microsoft.com microsoftonline.com sharepoint.com office.com office.net "
+## Crusaders Quest
+DIRECT += "cq.hangame.com cq-pvp.hangame.com cq-cha.hangame.com gslb-gamebase.nhncloudservice.com " # ios
+DIRECT += ""
+urls_direct.extend(DIRECT.split(' '))
 
-## do not need to change
-# Games
-# s = 'amp-api-edge.apps.apple.com push.apple.com inappcheck.itunes.apple.com app-measurement.com nexoncdn.co.kr nexon.com nexon.io'
-# urls_proxy.extend(s.split(' '))
-
-# Ghelper Default
-s = 'youtube.com googlevideo.com ytimg.com youtu.be yimg.com behance.net instagram.com t.co facebook.com instagram.com t.co ebay.co yahoo.co.jp google.com google.de google.fr google.com.hk google.com.sg google.com.tw google.co.jp google.com.za gstatic.com googleusercontent.com gvt2.com gvt3.com ggpht.com googleapis.com chrome.com googleadservices.com googleusercontent.com googlesyndication.com googlesource.com wikipedia.org pbskids.org dropbox.com googleblog.com appspot.com android.com github.com gmail.com wikimedia.org googlegroups.com githubusercontent.com arxiv.org mediawiki.org instagram.com t.co'
-urls_proxy.extend(s.split(' '))
+PROXY = "amp-api-edge.apps.apple.com push.apple.com inappcheck.itunes.apple.com app-measurement.com nexoncdn.co.kr nexon.com nexon.io "
+# PROXY
+urls_proxy.extend(PROXY.split(' '))
 
 urls_proxy = set(urls_proxy)
 urls_direct = set(urls_direct)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # if mode == 'clash':
 #     res = requests.get("https://ghelper.me/clash/4ff905a0af778432e7dd12dfe21788cb")
@@ -34,10 +50,11 @@ urls_direct = set(urls_direct)
 #     s = '# default\n- DOMAIN-SUFFIX,ip-api.com,DIRECT\n- DOMAIN-SUFFIX,ipip.net,DIRECT\n- DOMAIN-SUFFIX,ip138.com,DIRECT\n- DOMAIN-SUFFIX,stunnel.vip,DIRECT\n- DOMAIN-SUFFIX,gotochinatown.net,DIRECT\n- DOMAIN-SUFFIX,ghelper.net,DIRECT\n- DOMAIN-SUFFIX,ghelper.me,DIRECT\n- DOMAIN-SUFFIX,ghelper.xyz,DIRECT\n- DOMAIN-SUFFIX,ghelper.org,DIRECT\n- DOMAIN-SUFFIX,fastapi.me,DIRECT\n- DOMAIN-SUFFIX,vps315.com,DIRECT\n- DOMAIN-SUFFIX,copyplay.net,DIRECT\n- DOMAIN-SUFFIX,pickdown.net,DIRECT\n- DOMAIN-KEYWORD,google,Ghelper\n- DOMAIN-KEYWORD,youtube,Ghelper\n- DOMAIN-SUFFIX,ggpht.com,Ghelper\n- DOMAIN-SUFFIX,gmail.com,Ghelper\n- DOMAIN-SUFFIX,gvt2.com,Ghelper\n- DOMAIN-SUFFIX,gvt3.com,Ghelper\n- DOMAIN-SUFFIX,chrome.com,Ghelper\n- DOMAIN-SUFFIX,wikipedia.org,Ghelper\n- DOMAIN-SUFFIX,wikimedia.org,Ghelper\n- DOMAIN-SUFFIX,appspot.com,Ghelper\n- DOMAIN-SUFFIX,android.com,Ghelper\n- DOMAIN-SUFFIX,github.com,Ghelper\n- DOMAIN-SUFFIX,gitbook.com,Ghelper\n- DOMAIN-SUFFIX,kaggle.com,Ghelper\n- DOMAIN-SUFFIX,arxiv.org,Ghelper\n- DOMAIN-SUFFIX,wiktionary.org,Ghelper\n- DOMAIN-SUFFIX,blogger.com,Ghelper\n- DOMAIN-SUFFIX,youtu.be,Ghelper\n- DOMAIN-SUFFIX,ytimg.com,Ghelper\n- DOMAIN-SUFFIX,youtube.com,Ghelper\n- DOMAIN-SUFFIX,instagram.com,Ghelper\n- DOMAIN-SUFFIX,twitter.com,Ghelper\n- DOMAIN-SUFFIX,t.co,Ghelper\n- DOMAIN-SUFFIX,facebook.com,Ghelper\n- DOMAIN-SUFFIX,telegram.org,Ghelper\n- IP-CIDR,192.168.0.0/16,DIRECT\n- IP-CIDR,10.0.0.0/8,DIRECT\n- IP-CIDR,172.16.0.0/12,DIRECT\n- IP-CIDR,172.23.0.0/12,DIRECT\n- IP-CIDR,127.0.0.0/8,DIRECT\n- IP-CIDR,100.64.0.0/10,DIRECT\n- IP-CIDR6,::1/128,DIRECT\n- IP-CIDR6,fc00::/7,DIRECT\n- IP-CIDR6,fe80::/10,DIRECT\n- IP-CIDR6,fd00::/8,DIRECT\n- GEOIP,CN,DIRECT\n- MATCH,DIRECT'
 #     print(s)
 
-if mode == 'quan x':
+if MODE == "quan x":
     for token in urls_proxy:
         print('host-suffix,{},proxy'.format(token))
     for token in urls_direct:
         print('host-suffix,{},direct'.format(token))
     # s = 'ip-cidr, 10.0.0.0/8, direct\nip-cidr, 127.0.0.0/8, direct\nip-cidr, 172.16.0.0/12, direct\nip-cidr, 192.168.0.0/16, direct\nip-cidr, 224.0.0.0/24, direct\ngeoip, cn, direct\nfinal, direct'
-    print(s)
+if MODE == "nekoray":
+    pass
