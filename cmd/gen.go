a1ace7b2af8f7de3b0b5c314a31b8a1c85674495
Ricebucket <88389934+r1cebucket@users.noreply.github.com>
2023-02-11 12:11:23 +0000

    update

--- cmd/gen.go
+++ cmd/gen.go
@@ -8,8 +8,7 @@ import (
 
 func main() {
 	var MODES []string
-	var domainProxy []string
-	var domainDirect []string
+	var domainProxy, domainDirect []string
 
 	MODES = []string{
 		"clash",
@@ -23,12 +22,15 @@ func main() {
 	// PROXY
 	PROXY := "amp-api-edge.apps.apple.com push.apple.com inappcheck.itunes.apple.com app-measurement.com nexoncdn.co.kr nexon.com nexon.io "
 	// Microsoft
-	PROXY += "windows.com windows.net office.com microsoft.com "
-	PROXY += "contentsync.onenote.com hierarchyapi.onenote.com www.onenote.com "
+	{
+		PROXY += "windows.com windows.net office.com microsoft.com "
+		PROXY += "contentsync.onenote.com hierarchyapi.onenote.com www.onenote.com "
+	}
 	// Apple
 	PROXY += "app.adjust.com "
 	// Crusaders Quest
-	PROXY += "cq.hangame.com cq-pvp.hangame.com cq-cha.hangame.com "
+	PROXY += "hangame.com "
+	// PROXY += "cq.hangame.com cq-pvp.hangame.com cq-cha.hangame.com "
 	PROXY += "nhn.com gslb-gamebase.nhncloudservice.com toast.com "
 
 	// DIRECT
@@ -36,7 +38,9 @@ func main() {
 	// Microsoft
 	DIRECT += "microsoftonline.com sharepoint.com office.net live.com onenote.com "
 	// Crusaders Quest
-	DIRECT += "nhnst.com cru.cdn.toastoven.net adam.gslb.toastoven.net api-iaptacc.gslb.toastoven.net "
+	DIRECT += "nhnst.com "
+	DIRECT += "toastoven.net "
+	// DIRECT += "cru.cdn.toastoven.net adam.gslb.toastoven.net api-iaptacc.gslb.toastoven.net "
 	DIRECT += "unity3d.com "
 	domainProxy = strings.Split(PROXY, " ")
 	domainDirect = strings.Split(DIRECT, " ")
