e1a20411738d22b920c75283e6719f51d93a3194
Ricebucket <88389934+r1cebucket@users.noreply.github.com>
2024-01-07 13:54:35 +0000

    update

--- cmd/gen.go
+++ cmd/gen.go
@@ -6,12 +6,55 @@ import (
 	"strings"
 )
 
+const (
+	SHADOWROCKET_PREFIX = `
+[General]
+bypass-system = true
+skip-proxy = 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, localhost, *.local, captive.apple.com
+tun-excluded-routes = 10.0.0.0/8, 100.64.0.0/10, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.0.0.0/24, 192.0.2.0/24, 192.88.99.0/24, 192.168.0.0/16, 198.51.100.0/24, 203.0.113.0/24, 224.0.0.0/4, 255.255.255.255/32, 239.255.255.250/32
+dns-server = system
+fallback-dns-server = system
+ipv6 = true
+prefer-ipv6 = false
+dns-fallback-system = false
+dns-direct-system = false
+icmp-auto-reply = true
+always-reject-url-rewrite = false
+private-ip-answer = true
+# direct domain fail to resolve use proxy rule
+dns-direct-fallback-proxy = true
+# The fallback behavior when UDP traffic matches a policy that doesn't support the UDP relay. Possible values: DIRECT, REJECT.
+udp-policy-not-supported-behaviour = REJECT
+
+[Rule]
+`
+	SHADOWROCKET_SUFFIX = `
+# LAN
+IP-CIDR,192.168.0.0/16,DIRECT
+IP-CIDR,10.0.0.0/8,DIRECT
+IP-CIDR,172.16.0.0/12,DIRECT
+IP-CIDR,127.0.0.0/8,DIRECT
+# China
+GEOIP,CN,DIRECT
+# Final
+FINAL,PROXY
+
+[Host]
+localhost = 127.0.0.1
+
+[URL Rewrite]
+^https?://(www.)?g.cn https://www.google.com 302
+^https?://(www.)?google.cn https://www.google.com 302
+`
+)
+
 func main() {
 	MODES := []string{
 		"clash",
 		"quan x",
 		"matsuri",
 		"surge",
+		"shadowrocket",
 	}
 
 	var domainProxy, domainDirect, domainReject []string
@@ -246,6 +289,31 @@ func SaveConfig(domainReject, domainProxy, domainDirect []string, MODE string) {
 			rule = fmt.Sprintf("DOMAIN-SUFFIX,%s\n", domain)
 			confDirect.Write([]byte(rule))
 		}
+	case "shadowrocket":
+		conf, err := os.Create("./rules/shadowrocket.conf")
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		defer conf.Close()
+
+		conf.Write([]byte(SHADOWROCKET_PREFIX))
+
+
+		for _, domain := range domainReject[:len(domainReject)-1] {
+			rule = fmt.Sprintf("DOMAIN-SUFFIX,%s,REJECT\n", domain)
+			conf.Write([]byte(rule))
+		}
+		for _, domain := range domainProxy[:len(domainProxy)-1] {
+			rule = fmt.Sprintf("DOMAIN-SUFFIX,%s,PROXY\n", domain)
+			conf.Write([]byte(rule))
+		}
+		for _, domain := range domainDirect[:len(domainDirect)-1] {
+			rule = fmt.Sprintf("DOMAIN-SUFFIX,%s,DIRECT\n", domain)
+			conf.Write([]byte(rule))
+		}
+
+		conf.Write([]byte(SHADOWROCKET_SUFFIX))
 	default:
 		fmt.Println("no such mode:", MODE)
 	}
